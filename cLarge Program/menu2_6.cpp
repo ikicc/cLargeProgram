c7f86adf76c4ed040c5d8ada589dcd1031990340
CompSci-001 <CompSci-001@PC27>
2023-05-30 11:48:53 +0800

    完成至2.6

--- cLarge Program/menu2_6.cpp
+++ cLarge Program/menu2_6.cpp
@@ -1,40 +1,5 @@
 #include <stdio.h>
 #include <iostream>
-/*void dispMenu2_6(void) {
-	local(2, 20);  printf("C数组结构\n");
-	local(1, 24);  printf("1. 数组的定义\n");
-	colum_n(24);  printf("2. 数组的初始化\n");
-	colum_n(24);  printf("3. 数组的引用\n");
-	colum_n(24);  printf("4. 数组的输入输出\n");
-	colum_n(24);  printf("5. 一维数组\n");
-	colum_n(24);  printf("6. 二维数组\n");
-	colum_n(24);  printf("7. 字符数组\n");
-	local(2, 24);  printf("0. 返回\n");
-	local(2, 20);  printf("请选择：");
-}
-*/
-
-//按顺序介绍以上知识点
-/*void m2_5_1(void) {   //1.函数的定义
-	system("cls");
-	printf("\n\n    函数的定义\n\n");
-	printf("函数定义的一般形式为：\n");
-	printf("类型说明符 函数名(参数表)\n");
-	printf("{\n");
-	printf("    函数体\n");
-	printf("}\n");
-	printf("注：函数名是标识符，参数表可以为空。\n");
-	printf("例：\n");
-	printf("int max(int a,int b)\n");
-	printf("{\n");
-	printf("    if(a>b)\n");
-	printf("        return a;\n");
-	printf("    else\n");
-	printf("        return b;\n");
-	printf("}\n");
-	printf("注：max是函数名，a和b是参数，函数体中的语句是函数的功能。\n");
-	getchar();
-*/
 void m2_6_1(void) {   //1.数组的定义
 	system("cls");
 	printf("\n\n    数组的定义\n\n");
